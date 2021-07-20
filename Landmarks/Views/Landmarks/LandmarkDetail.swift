efd28600f4c983a32dbe1e99e798be0f9588ebc9
johnturner4004 <johnturner4004@gmail.com>
2021-07-20 19:13:46 -0400

    add toggle for favorites

--- Landmarks/Views/Landmarks/LandmarkDetail.swift
+++ Landmarks/Views/Landmarks/LandmarkDetail.swift
@@ -8,7 +8,12 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+    @EnvironmentObject var modelData: ModelData
     var landmark: Landmark
+    
+    var landmarkIndex: Int {
+        modelData.landmarks.firstIndex(where: { $0.id == landmark.id })!
+    }
     
     var body: some View {
         ScrollView {
@@ -21,8 +26,12 @@ struct LandmarkDetail: View {
                 .padding(.bottom, -130)
             
             VStack(alignment: .leading) {
-                Text(landmark.name)
-                    .font(.title)
+                HStack {
+                    Text(landmark.name)
+                        .font(.title)
+                        .foregroundColor(.primary)
+                    FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+                }
                 
                 HStack {
                     Text(landmark.park)
@@ -33,9 +42,9 @@ struct LandmarkDetail: View {
                 .foregroundColor(.secondary)
                 
                 Divider()
-                
+    
                 Text("About \(landmark.name)")
-                    .font(.title2)
+                        .font(.title2)
                 Text(landmark.description)
             }
             .padding()
@@ -48,9 +57,9 @@ struct LandmarkDetail: View {
 }
 
 struct LandmarkDetail_Previews: PreviewProvider {
-    static var landmarks = ModelData().landmarks
+    static let modelData = ModelData()
     
     static var previews: some View {
-        LandmarkDetail(landmark: landmarks[0])
+        LandmarkDetail(landmark: modelData.landmarks[0])
     }
 }
